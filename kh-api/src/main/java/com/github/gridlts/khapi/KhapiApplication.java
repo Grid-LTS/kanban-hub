6f0f30bfb746da6f2210bc4c36bf2ed4fe489f52
Gerd Friemel <gerd.friemel@tngtech.com>
2019-02-15 18:25:36 +0100

    Initial commit with Spring boot scaffold.

---
+++ kh-api/src/main/java/com/github/gridlts/khapi/KhapiApplication.java
@@ -0,0 +1,16 @@
+package com.github.gridlts.khapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.github.gridlts.khapi.web.controller"})
+@SpringBootApplication
+public class KhapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhapiApplication.class, args);
+	}
+
+}
+
